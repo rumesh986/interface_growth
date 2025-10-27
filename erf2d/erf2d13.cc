5522fc88a81735d8f47591efaf84d5207520b009
Rumesh Sudhaharan <rumesh986@gmail.com>
2025-10-27 16:37:15 +0000

    erf2d13 cleanup and add fd check for flux

--- erf2d/erf2d13.cc
+++ erf2d/erf2d13.cc
@@ -26,12 +26,11 @@ bool ic_set = false;
 
 static const double T_s = -1.0;
 static const double T_m = 0.0;
-// static const double T_l = 1.0;
-static const double T_l = T_m;
+static const double T_l = 1.0;
 static const double T_p = (T_l - T_m) / (T_l - T_s);
 
-double xs[3] = {0.0, 0.025, 1.0};
-static const double ys[2] = {0.0, 1.0};
+double xs[3] = {0.0, 0.025, 3.0};
+static const double ys[2] = {0.0, 0.0001};
 
 FreeBoundaryElement *geometry;
 
@@ -60,7 +59,6 @@ void get_exact_u(const double &t, const Vector<double> &x, Vector<double> &u) {
 	if (x[0] < h) {
 		double erf_zeta = erf(0.5 * x[0] / sqrt(D[0] * t));
 		double erf_iface = erf(0.5 * h / sqrt(D[0]*t));
-		// u[0] = ((T_p - 1.0)*erf_zeta + T_p + erf_iface) / (1.0 + erf_iface);
 		u[0] = 1.0 + (T_p - 1.0) * erf_zeta/erf_iface;
 	} else {
 		double erf_zeta = erf(0.5  * x[0] / sqrt(D[1] * t));
@@ -81,11 +79,10 @@ void get_exact_u(const double &t, const Vector<double> &x, Vector<double> &u) {
 
 Vector<unsigned int> analytical_boundaries = {
 	1, // right
-	// 3, // left
+	3, // left
 };
 
 Vector<unsigned int> pinned_boundaries = {
-	3, // left
 	4 // interface
 };
 
@@ -107,19 +104,25 @@ class Erf2DProblem : public Problem {
 		TwoPhaseFreeBoundarySpineMesh<SpineElement<EL>> *bulk_mesh_pt;
 		Mesh *geometry_mesh_pt;
 
+		TimeStepper *iface_ts_pt;
+
 		public:
 		Erf2DProblem(
-			unsigned int nx_,
+			unsigned int nx1_,
+			unsigned int nx2_,
 			unsigned int ny_,
 			unsigned int tsteps,
 			double dt_,
 			double tshift,
 			DocInfo info_
-		) : nx1(nx_), nx2(nx_), nx(nx_+nx_), ny(ny_), t_steps(tsteps), dt(dt_), t_shift(tshift), info(info_) {
+		) : nx1(nx1_), nx2(nx2_), nx(nx1_+nx2_), ny(ny_), t_steps(tsteps), dt(dt_), t_shift(tshift), info(info_) {
 
 			add_time_stepper_pt(new BDF<T_ORDER>);
 
-			geometry = new FreeBoundaryElement(xs[0], xs[1], xs[2], ys[0], ys[1], St, time_stepper_pt());
+			iface_ts_pt = new BDF<1>;
+			add_time_stepper_pt(iface_ts_pt);
+
+			geometry = new FreeBoundaryElement(xs[0], xs[1], xs[2], ys[0], ys[1], St, iface_ts_pt);
 
 			printf("x0=%8.6f x1=%8.6f x2=%8.6f\n", geometry->x0(), geometry->x1(), geometry->x2());
 
@@ -195,11 +198,10 @@ class Erf2DProblem : public Problem {
 		// void actions_before_newton_solve() {};
 		void actions_after_newton_solve() {};
 
-		// void actions_before_implicit_timestep() {
-		void actions_before_newton_solve() {
+		void actions_before_implicit_timestep() {
 			Vector<double> x(2), u(2);
 
-			double time = time_pt()->time();
+			double time = time_pt()->time() + dt;
 
 			for (unsigned int b : analytical_boundaries) {
 				unsigned long int nnode = bulk_mesh_pt->nboundary_node(b);
@@ -214,34 +216,52 @@ class Erf2DProblem : public Problem {
 
 		// void actions_before_newton_convergence_check() {
 		void actions_before_newton_step() {
-			Vector<double> flux(2);
+			// set all node values according to bottom row of nodes
+			// for (unsigned int e = 0; e < nx; e++) {
+			// 	FiniteElement *elem = bulk_mesh_pt->finite_element_pt(e);
+			// 	// elem->node_pt(2)->set_value(0, elem->nodal_value(0, 0));
+			// 	// elem->node_pt(3)->set_value(0, elem->nodal_value(1, 0));
+			// 	for (unsigned int n1 = 0; n1 < elem->nnode_1d(); n1++) {
+			// 		for (unsigned int n2 = 1; n2 < elem->nnode_1d(); n2++) {
+			// 			elem->node_pt(n1 + elem->nnode_1d() * n2)->set_value(0, elem->nodal_value(n1, 0));
+			// 		}
+			// 	}
+			// }
+			
+			Vector<double> flux(2), flux_fd(2);
 
 			double tot_flux = 0.0;
+			double tot_flux_fd = 0.0;
 			unsigned long int nelems = bulk_mesh_pt->nboundary_element(4);
+
+			Vector<double> s(2);
+			s[1] = 0.0;
+			double factor = 0.0;
+
 			for (unsigned long int e = 0; e < nelems; e++) {
 				int face_index = bulk_mesh_pt->face_index_at_boundary(4, e);
+				EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->boundary_element_pt(4, e));
+
 				if (face_index == 1) {
-					Vector<double> s = {1.0, 0.0};
-
-					EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->boundary_element_pt(4, e));
-					elem->get_flux(s, flux);
-
-					tot_flux -= D[0] * flux[0];
+					s[0] = 1.0;
+					factor = -D[0];
 				} else if (face_index == -1) {
-					Vector<double> s = {-1.0, 0.0};
-
-					EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->boundary_element_pt(4, e));
-					elem->get_flux(s, flux);
-
-					tot_flux += D[0] * k[1] / k[0] * flux[0];
+					s[0] = -1.0;
+					factor = D[0] * k[1] / k[0];
 				}
-			}
+				elem->get_flux(s, flux);
+				get_flux_fd(0, elem, flux_fd, face_index);
 
-			printf("Setting total flux to %8.6f interface at %16.14f\n", tot_flux / ny, geometry->get_interface());
+				tot_flux += factor * flux[0];
+				tot_flux_fd += factor * flux_fd[0];
+			}
+			
+			printf("Setting total flux to %8.6f (fd: %8.6f) interface at %16.14f\n", tot_flux / ny, tot_flux_fd / ny, geometry->get_interface());
 			geometry->set_flux(tot_flux / ny);
 		}
 
-		void actions_after_newton_step() {
+		// void actions_after_newton_step() {
+		void actions_before_newton_convergence_check() {
 			for (unsigned s = 0; s < bulk_mesh_pt->nspine(); s++) {
 				bulk_mesh_pt->spine_pt(s)->height() = geometry->x1();
 			}
@@ -259,10 +279,13 @@ class Erf2DProblem : public Problem {
 			Vector<double> flux(2);
 			Vector<double> r(2);
 
+			unsigned int tsteps = time_stepper_pt()->nprev_values();
+			double start_iface_x = sqrt(D[2] * time_pt()->time(tsteps));
+			geometry->set_interface(start_iface_x);
+			bulk_mesh_pt->node_update();
+
 			for (unsigned long int n = 0; n < nnode; n++)
 				time_stepper_pt()->assign_initial_positions_impulsive(bulk_mesh_pt->node_pt(n));
-			
-			unsigned int tsteps = time_stepper_pt()->nprev_values();
 
 			for (unsigned long int n = 0; n < nnode; n++) {
 				bulk_mesh_pt->node_pt(n)->position(tsteps, x);
@@ -306,14 +329,14 @@ class Erf2DProblem : public Problem {
 					new_h = sqrt(D[2] * time);
 				}
 
-				geometry->set_interface(new_h);
 				printf("IC %u: x_old=%8.6f x_new=%8.6f\n", t, geometry->get_interface(), new_h);
+				geometry->set_interface(new_h);
 				bulk_mesh_pt->node_update();
 
 				for (unsigned long int n = 0; n < nnode; n++) {
 					bulk_mesh_pt->node_pt(n)->position(t, x);
 					get_exact_u(time, x, u);
-					bulk_mesh_pt->node_pt(n)->set_value(t, 0, u[0]);
+					bulk_mesh_pt->node_pt(n)->set_value(t, 0, u[1]);
 				}
 
 				printf("[% 4d] Setting initial condition at t=%8.6f\n", step, time);
@@ -358,9 +381,10 @@ class Erf2DProblem : public Problem {
 			double max_elem_size, min_elem_size;
 			bulk_mesh_pt->max_and_min_element_size(max_elem_size, min_elem_size);
 
+			// printf("Max element size: %16.14f min element size: %16.14f\n", max_elem_size, min_elem_size);
 			sprintf(fname, "%s/results.dat", info.directory().c_str());
 			file = fopen(fname, "a");
-			fprintf(file, "%16.14f %16.14f %16.14f %16.14f %16.14f\n", time, tot_error, geometry->get_interface(), sqrt(D[2]*time), max_elem_size);
+			fprintf(file, "%16.14f %16.14f %16.14f %16.14f %16.14f\n", time, tot_error, geometry->get_interface(), sqrt(D[2]*time), max_elem_size/ys[1]);
 			fclose(file);
 
 			printf("[%4u] time=%8.6f error=%e iface_err=%e interface=%8.6f expected=%8.6f\n", timestep, time, tot_error, fabs(geometry->get_interface() - sqrt(D[2] * time)), geometry->get_interface(), sqrt(D[2]*time));
@@ -395,12 +419,52 @@ class Erf2DProblem : public Problem {
 				}
 			}
 		}
+
+		void get_flux_fd(const unsigned &t, EL *elem, Vector<double> &flux, int direction) {
+			unsigned n_node = elem->nnode_1d();
+			Vector<Vector<double>> pos(n_node);
+			Vector<double> fluxes(n_node);
+			Vector<Node *> nodes(n_node);
+
+			for (unsigned int i = 0; i < n_node; i++) pos[i].reserve(2);
+
+			// n1 is to loop in y-direction
+			// n2 is main loop in x-direciton
+			for (unsigned int n1 = 0; n1 < n_node; n1++) {
+				for (unsigned int n2 = 0; n2 < n_node; n2++) {
+					nodes[n2] = elem->node_pt(n1 * n_node + n2);
+					nodes[n2]->position(t, pos[n2]);
+				}
+
+				double dx = pos[n_node-1][0] - pos[0][0];
+				double dT = 0.0;
+				
+				if (direction > 0) {
+					// solid phase 
+					switch(n_node) {
+						case 2: dT = - nodes[0]->value(0, t) +     nodes[1]->value(0, t);															break;
+						case 3: dT =   nodes[0]->value(0, t) - 4.0*nodes[1]->value(0, t) + 3.0*nodes[2]->value(0, t);								break;
+						case 4: dT = - nodes[0]->value(0, t) + 4.5*nodes[1]->value(0, t) - 9.0*nodes[2]->value(0, t) + 5.5*nodes[3]->value(0, t);	break;
+					}
+				} else {
+					// liquid phase
+					switch (n_node) {
+						case 2: dT =  nodes[1]->value(0, t) -     nodes[0]->value(0, t);															break;
+						case 3: dT = -nodes[2]->value(0, t) + 4.0*nodes[1]->value(0, t) - 3.0*nodes[0]->value(0, t);								break;
+						case 4: dT =  nodes[3]->value(0, t) - 4.5*nodes[2]->value(0, t) + 9.0*nodes[1]->value(0, t) - 5.5*nodes[0]->value(0, t);	break;
+					}
+				}
+				fluxes[n1] = dT / dx;
+			}
+			flux[0] = fluxes[0];
+		}
 };
 
 int main(int argc, char **argv) {
 	CommandLineArgs::setup(argc, argv);
 
-	uint Nx = 0;
+	uint Nx1 = 0;
+	uint Nx2 = 0;
 	uint Ny = 10;
 	uint t_steps = 100;
 	double dt = 0.0;
@@ -409,7 +473,7 @@ int main(int argc, char **argv) {
 	uint write_freq = 1;
 	std::string dname;
 
-	CommandLineArgs::specify_command_line_flag("--nx", &Nx);
+	CommandLineArgs::specify_command_line_flag("--nx", &Nx1);
 	CommandLineArgs::specify_command_line_flag("--ny", &Ny);
 	CommandLineArgs::specify_command_line_flag("--tsteps", &t_steps);
 	CommandLineArgs::specify_command_line_flag("--dt", &dt);
@@ -430,7 +494,7 @@ int main(int argc, char **argv) {
 
 	var_dt = CommandLineArgs::command_line_flag_has_been_set("--vardt");
 
-	if (Nx == 0) {
+	if (Nx1 == 0) {
 		cout << "Error: Nx not specified" << endl;
 		exit(1);
 	}
@@ -446,12 +510,12 @@ int main(int argc, char **argv) {
 			exit(1);
 		}
 
-		dt = pow(((double)1/(double)Nx), 2);
+		dt = pow(((double)1/(double)Nx1), 2);
 	}
 
 	if (!CommandLineArgs::command_line_flag_has_been_set("--dname")) {
 		char temp[256];
-		sprintf(temp, "RESLT/%dn%u_%dt%.2e", X_ORDER, Nx, T_ORDER, dt);
+		sprintf(temp, "RESLT/%dn%u_%dt%.2e", X_ORDER, Nx1, T_ORDER, dt);
 		dname.assign(temp);
 	}
 
@@ -497,7 +561,8 @@ int main(int argc, char **argv) {
 	printf("\tx0=%8.6f x1=%8.6f x2=%8.6f\n", xs[0], xs[1], xs[2]);
 	printf("\tL=%8.6f St=%8.6f\n", L, St);
 
-	auto problem = Erf2DProblem<QUnsteadyHeatElement<2,X_ORDER>>(Nx, Ny, t_steps, dt, t_shift, info);
+	Nx2 = Nx1;
+	auto problem = Erf2DProblem<QUnsteadyHeatElement<2,X_ORDER>>(Nx1, Nx2, Ny, t_steps, dt, t_shift, info);
 
 	problem.initialise_dt(dt);
 	problem.set_initial_condition();
@@ -507,9 +572,9 @@ int main(int argc, char **argv) {
 	sprintf(config_fname, "%s/config", dname.c_str());
 
 	FILE *file = fopen(config_fname, "w");
-	fprintf(file, "nx1=%d\n", Nx);
-	fprintf(file, "nx2=%d\n", Nx);
-	fprintf(file, "nx=%d\n", Nx+Nx);
+	fprintf(file, "nx1=%d\n", Nx1);
+	fprintf(file, "nx2=%d\n", Nx2);
+	fprintf(file, "nx=%d\n", Nx1+Nx2);
 	fprintf(file, "x_order=%d\n", X_ORDER);
 	fprintf(file, "dt=%e\n", dt);
 	fprintf(file, "t_shift=%e\n", t_shift);
@@ -523,6 +588,7 @@ int main(int argc, char **argv) {
 	fprintf(file, "cp1=%10.8f\n", Cp[0]);
 	fprintf(file, "cp2=%10.8f\n", Cp[1]);
 	fprintf(file, "L=%10.8f\n", L);
+	fprintf(file, "De=%10.8f\n", D[2]);
 	fclose(file);
 
 	int prev_steps = problem.time_stepper_pt()->nprev_values()+1;
@@ -530,7 +596,7 @@ int main(int argc, char **argv) {
 	for (uint t = 0; t < t_steps; t++) {
 		problem.unsteady_newton_solve(dt);
 
-		if (t % write_freq == 0)
+		if (t % write_freq == 0 || t == t_steps-1)
 			problem.doc_step(t+prev_steps);
 
 		double x_int = geometry->get_interface();
