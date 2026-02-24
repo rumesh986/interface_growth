280fed7a5b5c4504c99ec5e03365fd93a8970b3a
Rumesh Sudhaharan <rumesh986@gmail.com>
2026-02-24 11:22:38 +0000

    add classes for running things locally

---
+++ one_dim/setup/build.py
@@ -0,0 +1,74 @@
+import os
+import subprocess
+from datetime import datetime
+from time import sleep
+
+from concurrent.futures import ProcessPoolExecutor
+
+from systems.systems import System, SimParams, Params, AnalysisType
+
+class Build:
+	def __init__(self, 
+		prog: str,
+		params: SimParams,
+		source_dir: str,
+		system: System,
+		dname: str | None = None,
+		workspacesf: str = 'workspaces',
+		debug: bool = False,
+		interactive: bool = False,
+		nthreads: int = os.cpu_count()
+	) -> None:
+		self.prog = prog
+		self.params = params
+		self.source_dir = source_dir
+		self.system = system
+		self.nthreads = nthreads
+
+		if dname is None:
+			dname = f'{self.prog}_{datetime.today().strftime("%Y-%m-%d_%H:%M:%S")}'
+
+		self.od = os.getcwd()
+		self.wd = f'{self.od}/{workspacesf}/{dname}'
+
+		if not os.path.exists(f'{self.od}/{workspacesf}'):
+			os.mkdir(f'{self.od}/{workspacesf}')
+
+	def build_all(self) -> None:
+		os.chdir(self.source_dir)
+
+		self.bins = {}
+
+		command = ['cmake', '--build', 'build', '--target']
+		for x in self.params.xs:
+			for t in self.params.ts:
+				bin_name = f'{self.prog}_{x}_{t}'
+				command.append(bin_name)
+				self.bins[(x, t)] = bin_name
+		
+		result = subprocess.run(command)
+		if result.returncode != 0:
+			raise Exception(f"Build failed with error code {result.returncode}")
+
+		if not os.path.exists(self.wd):
+			os.mkdir(self.wd)
+
+		for item in self.bins.values():
+			os.rename(f'build/{item}', f'{self.wd}/{item}')
+	
+	def process_args(self, params: Params, *args, **kwargs) -> list[str]:
+		ret = [
+			*params.args,
+			*self.system.args
+		]
+
+		if len(args) > 0:
+			print(args)
+			print(len(args))
+			ret.extend(args)
+		
+		for k, v in kwargs.items():
+			print(f'{k}={v}')
+			ret.extend([f'--{k}', str(v)])
+
+		return ret
---
+++ one_dim/setup/local.py
@@ -0,0 +1,66 @@
+import os
+import subprocess
+from datetime import datetime
+from time import sleep
+
+from concurrent.futures import ProcessPoolExecutor
+
+from setup.build import Build
+from systems.systems import System, SimParams, Params, AnalysisType
+
+class RunLocal(Build):
+	def run(self, params: Params, *args, **kwargs) -> None:
+		try:
+			cmd_args = self.process_args(params, *args, **kwargs)
+
+			with open(f'{self.wd}/{self.bins[(params.x, params.t)]}_{params.short_title}.stdout', 'w') as f:
+				cmd = [f'./{self.bins[(params.x, params.t)]}', *cmd_args]
+				f.write(f'COMMAND: {cmd}\n\n')
+				f.flush()
+				subprocess.run(
+					cmd,
+					check=True,
+					stderr=subprocess.STDOUT,
+					stdout=f
+				)
+		except subprocess.CalledProcessError as e:
+			print(f'run failed with params: {params}')
+			with open(f'{self.wd}/{self.bins[(params.x, params.t)]}.stdout', 'r') as f:
+				print(f.read())
+			raise
+
+
+	def run_all(self, *args, **kwargs) -> None:
+		num_workers = self.params.num_jobs
+		if (num_workers > self.nthreads):
+			num_workers = self.nthreads
+		
+		os.chdir(self.wd)
+		self.params.to_file('params')
+
+		futures = {}
+		with ProcessPoolExecutor(max_workers=num_workers) as executor:
+			match self.params.analysis_type:
+				case AnalysisType.standard | AnalysisType.dx | AnalysisType.dt:
+					for params in self.params.jobs:
+						print(params)
+
+						futures[params] = executor.submit(self.run, params, *args, **kwargs)
+					
+				case _:
+					raise NotImplementedError()
+
+			while len(futures) > 0:
+				dellist = []
+				for params, future in futures.items():
+					if future.done():
+						print(f'Completed {params}')
+						if future.exception() is None:
+							dellist.append(params)
+						else:
+							raise future.exception()
+				
+				for params in dellist:
+					del futures[params]
+
+				sleep(5)
