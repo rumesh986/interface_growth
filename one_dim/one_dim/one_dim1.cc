0779609770d450e96a51810c37686f750d847a1e
Rumesh Sudhaharan <rumesh986@gmail.com>
2026-02-27 15:40:37 +0000

    add initial code for one_dim simulations

---
+++ one_dim/one_dim/one_dim1.cc
@@ -0,0 +1,466 @@
+// moving interface
+// calcualte interface position as part of solution in newton iterations
+// move nodes with spine mesh
+// use proper non-dimensionalised and corrected temperature scaling
+// 		uses the form with separate alpha/beta constants
+// interface temperature is not pinned, adds flux contributions for Stefan condition
+// still accepts dimensinoal inputs and outputs non-dimensional values
+
+#include <cmath>
+#include <filesystem>
+
+#include "generic.h"
+#include "unsteady_heat.h"
+// #include "unsteady_heat_flux_elements.h"
+
+#include "two_phase_free_boundary_mesh.h"
+
+using namespace oomph;
+
+namespace params {
+	struct material {
+		double k = NAN;
+		double rho = NAN;
+		double cp = NAN;
+		double D = NAN;
+	};
+
+	struct material solid;
+	struct material liquid;
+
+	double L = NAN;
+	double De = NAN;
+
+	double alpha = 0.0;
+	double beta = 0.0;
+	double D = 0.0;
+	double St = 0.0;
+
+	double T_s = NAN;
+	double T_m = NAN;
+	double T_l = NAN;
+
+	double xs[3] = {0.0, 0.5, 1.0};
+	double ys[2] = {0.0, 0.0001};
+
+	unsigned int nxs[2] = {10, 10};
+	unsigned int nx = 0;
+	unsigned int ny = 1;
+	double dt = 0.01;
+	double tstart = 0.5;
+	double tend = 1.0;
+	unsigned int write_freq = 1;
+	std::string dname;
+
+	unsigned int tsteps;
+
+	void get_source(const double &t, const Vector<double> &x, double &source) {
+		source = 0.0;
+	}
+
+	void no_flux_fct(const double &t, const Vector<double> &x, double &flux) {
+		flux = 0.0;
+	}
+
+	void get_exact_u(const double &t, const Vector<double> &x, double &u) {
+		double h = sqrt(De * t);
+
+		if (x[0] < h) {
+			double denom = 0.5 / sqrt(t);
+			u = (erf(x[0] * denom) / erf(h * denom)) - 1.0;
+			// printf("solid: ");
+		} else {
+			double denom = 0.5 / sqrt(D * t);
+			double trans_tl = (T_l - T_m) / (T_m - T_s);
+			u = trans_tl * (erf(x[0] * denom) - erf(h * denom)) / (1.0 - erf(h * denom));
+			// printf("liquid: ");
+		}
+
+		// printf("x0=%16.14f x1=%16.14f u=%16.14f h=%16.14f De=%16.14f\n", x[0], x[1], u, h, De);
+	}
+
+	Vector<unsigned int> pinned_boundaries = {
+		3
+	};
+
+	Vector<unsigned int> analytical_boundaries = {
+		1
+	};
+
+	std::map<unsigned int, UnsteadyHeatEquations<X_ORDER>::UnsteadyHeatSourceFctPt> flux_boundaries = {
+
+	};
+
+}
+
+template<class EL>
+class OneDimStefanProblem : public Problem {
+	private:
+		FreeBoundaryElement *geometry;
+		TwoPhaseFreeBoundarySpineMesh<SpineElement<EL>> *bulk_mesh_pt;
+		Mesh *surf_mesh_pt;
+		Mesh *geom_mesh_pt;
+		DocInfo info;
+	
+	public:
+		OneDimStefanProblem() {
+			add_time_stepper_pt(new BDF<T_ORDER>(true));
+
+			bulk_mesh_pt = new TwoPhaseFreeBoundarySpineMesh<SpineElement<EL>>(params::nxs, params::ny, params::xs, params::ys, time_stepper_pt());
+			bulk_mesh_pt->setup_boundary_element_info();
+			add_sub_mesh(bulk_mesh_pt);
+			geometry = bulk_mesh_pt->geometry();
+
+			geom_mesh_pt = new Mesh;
+			geom_mesh_pt->add_element_pt(geometry);
+			add_sub_mesh(geom_mesh_pt);
+
+			unsigned int interface_boundary_index = bulk_mesh_pt->free_boundary_index();
+
+			surf_mesh_pt = new Mesh;
+			for (unsigned int e = 0; e < bulk_mesh_pt->nboundary_element(interface_boundary_index); e++) {
+				EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->boundary_element_pt(interface_boundary_index, e));
+				int face_index = bulk_mesh_pt->face_index_at_boundary(interface_boundary_index, e);
+				if (face_index == 1) {
+					auto flux_elem = new FreeBoundaryFluxElement<EL>(elem, face_index, params::St);
+
+					// each flux elem now has mapping for all boundary nodes
+					// need to remove the additional data if this works as expected
+					for (unsigned int n = 0; n < bulk_mesh_pt->nboundary_node(interface_boundary_index); n++) {
+						Node *node = bulk_mesh_pt->boundary_node_pt(interface_boundary_index, n);
+						Data *data = geometry->get_data_for_node(node);
+						flux_elem->add_node_data(node, data);
+					}
+
+					surf_mesh_pt->add_element_pt(flux_elem);
+				}
+			}
+			add_sub_mesh(surf_mesh_pt);
+			
+			build_global_mesh();
+
+			for (unsigned int e = 0; e < bulk_mesh_pt->nelement(); e++) {
+				dynamic_cast<EL *>(bulk_mesh_pt->element_pt(e))->source_fct_pt() = params::get_source;
+			}
+
+			for (unsigned int b : params::pinned_boundaries) {
+				for (unsigned int n = 0; n < bulk_mesh_pt->nboundary_node(b); n++) {
+					bulk_mesh_pt->boundary_node_pt(b, n)->pin_all();
+				}
+			}
+
+			for (unsigned int b : params::analytical_boundaries) {
+				for (unsigned int n = 0; n < bulk_mesh_pt->nboundary_node(b); n++) {
+					bulk_mesh_pt->boundary_node_pt(b, n)->pin_all();
+				}
+			}
+
+			for (auto iter : params::flux_boundaries) {
+				create_flux_elements(iter.first, iter.second);
+			}
+
+			for (unsigned int i = 0; i < params::ny; i++) {
+				unsigned int base = i * params::nx;
+
+				for (unsigned int e = params::nxs[0]; e < params::nx; e++) {
+					EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->element_pt(base + e));
+					elem->alpha_pt() = &params::alpha;
+					elem->beta_pt() = &params::beta;
+				}
+			}
+
+			printf("Number of equations: %lu\n", assign_eqn_numbers());
+			printf("NDOF: %lu\n", ndof());
+
+			linear_solver_pt()->disable_doc_time();
+			disable_info_in_newton_solve();
+			newton_solver_tolerance() = 5e-10;
+			max_newton_iterations() = 1e7;
+			max_residuals() = 1e3;
+
+			printf("Tolerance for newton solver: %e\n", newton_solver_tolerance());
+		}
+
+		~OneDimStefanProblem() {
+			delete bulk_mesh_pt;
+			delete surf_mesh_pt;
+			delete geom_mesh_pt;
+		}
+
+		void create_flux_elements(unsigned int b, UnsteadyHeatEquations<X_ORDER>::UnsteadyHeatSourceFctPt &flux_pt) {
+			for (unsigned int e = 0; e < bulk_mesh_pt->nboundary_element(b); e++) {
+				EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->boundary_element_pt(b, e));
+				int face_index = bulk_mesh_pt->face_index_at_boundary(b, e);
+				UnsteadyHeatFluxElement<EL> *flux_elem = new UnsteadyHeatFluxElement<EL>(elem, face_index);
+				flux_elem->flux_fct_pt() = flux_pt;
+				bulk_mesh_pt->add_element_pt(flux_elem);
+			}
+		}
+
+		void actions_before_implicit_timestep() {
+			Vector<double> x(2);
+			double u;
+			double time = time_pt()->time();
+
+			for (unsigned int b : params::analytical_boundaries) {
+				for (unsigned int n = 0; n < bulk_mesh_pt->nboundary_node(b); n++) {
+					bulk_mesh_pt->boundary_node_pt(b, n)->position(x);
+					params::get_exact_u(time, x, u);
+					bulk_mesh_pt->boundary_node_pt(b, n)->set_value(0, u);
+				}
+			}
+		}
+
+		void actions_before_newton_solve() {
+			time_stepper_pt()->set_predictor_weights();
+			geometry->calculate_predicted_values();
+
+			for (unsigned int s = 0; s < bulk_mesh_pt->nspine(); s++) {
+				double h_pred = geometry->geom_data_pt(s)->value(time_stepper_pt()->predictor_storage_index(), 0);
+				bulk_mesh_pt->spine_pt(s)->height() = h_pred;
+				// *geometry->geom_data_pt(s)->value_pt(0) = h_pred;
+				geometry->geom_data_pt(s)->set_value(0, h_pred);
+			}
+
+			bulk_mesh_pt->node_update();
+
+			for (unsigned int i = 0; i < params::ny; i++) {
+				unsigned int base = i * params::nx;
+
+				for (unsigned int e = params::nxs[0]; e < params::nx; e++) {
+					EL *elem = dynamic_cast<EL *>(bulk_mesh_pt->element_pt(base + e));
+					printf("yi=%u e=%u alpha=%8.6f beta=%8.6f\n", i, e, elem->alpha(), elem->beta());
+				}
+			}
+		}
+
+		void actions_before_newton_step() {};
+		void actions_before_newton_convergence_check() {}
+		void actions_after_newton_step() {};
+		void actions_after_newton_solve() {};
+		void actions_after_implicit_timestep() {};
+
+		void set_initial_condition() {
+			Vector<double> x(2);
+			double u;
+
+			unsigned int tsteps = time_stepper_pt()->nprev_values();
+			unsigned int nnode = bulk_mesh_pt->nnode();
+			unsigned int step = 0;
+
+			double time = params::tstart - tsteps * params::dt;
+			time_pt()->time() = time;
+			time_pt()->dt() = params::dt;
+
+			double h = sqrt(params::De * time);
+			for (unsigned int s = 0; s < bulk_mesh_pt->nspine(); s++) {
+				bulk_mesh_pt->spine_pt(s)->height() = h;
+				geometry->geom_data_pt(s)->set_value(0, h);
+			}
+			bulk_mesh_pt->node_update();
+
+			for (unsigned int n = 0; n < nnode; n++) {
+				bulk_mesh_pt->node_pt(n)->position(x);
+				params::get_exact_u(time, x, u);
+				bulk_mesh_pt->node_pt(n)->set_value(0, u);
+			}
+
+			printf("[%2u] Setting initial condition at t=%8.6f\n", step, time);
+			doc_step(step);
+			step++;
+
+			for (unsigned int t = 0; t < tsteps; t++) {
+				shift_time_values();
+				time += params::dt;
+
+				time_pt()->time() = time;
+				time_pt()->dt() = params::dt;
+
+				double h = sqrt(params::De * time);
+				for (unsigned int s = 0; s < bulk_mesh_pt->nspine(); s++) {
+					bulk_mesh_pt->spine_pt(s)->height() = h;
+					geometry->geom_data_pt(s)->set_value(0, h);
+				}
+				bulk_mesh_pt->node_update();
+
+				for (unsigned int n = 0; n < nnode; n++) {
+					bulk_mesh_pt->node_pt(n)->position(x);
+					params::get_exact_u(time, x, u);
+					bulk_mesh_pt->node_pt(n)->set_value(0, u);
+				}
+
+				printf("[%2u] Setting initial condition at t=%8.6f\n", step, time);
+				doc_step(step);
+				step++;
+			}
+
+			for (unsigned int t = 0; t < time_stepper_pt()->nprev_values(); t++) {
+				printf("h0 values: %u=%e\n", t, geometry->geom_data_pt(0)->value(t, 0));
+
+			}
+
+			time_pt()->time() = time;
+		}
+
+		double get_interface() {
+			return 0.0;
+		}
+
+		void set_doc_info(DocInfo input) {
+			info = input;
+		}
+
+		void doc_step(const unsigned int &timestep, const unsigned int &t = 0) {
+			double time = time_pt()->time();
+			unsigned long int nnode = bulk_mesh_pt->nnode();
+
+			Vector<double> x(2), num_u(1);
+			double ana_u;
+
+			double tot_error = 0.0;
+
+			char fname[256];
+			sprintf(fname, "%s/steps/step%u.dat", info.directory().c_str(), info.number());
+			FILE *file = fopen(fname, "w");
+			fprintf(file, "x y exact_u u error\n");
+			for (unsigned long int n = 0; n < nnode; n++) {
+				bulk_mesh_pt->node_pt(n)->position(t, x);
+				bulk_mesh_pt->node_pt(n)->value(t, num_u);
+				params::get_exact_u(time, x, ana_u);
+
+				double error = num_u[0] - ana_u;
+				tot_error += error * error;
+
+				fprintf(file, "%16.14f %16.14f %16.14f %16.14f %16.14f\n", x[0], x[1], ana_u, num_u[0], error);
+			}
+			fclose(file);
+
+			tot_error = sqrt(tot_error) / nnode;
+
+			double max_elem_size, min_elem_size;
+			bulk_mesh_pt->max_and_min_element_size(max_elem_size, min_elem_size);
+
+			double num_h = geometry->geom_data_pt(0)->value(0);
+
+			sprintf(fname, "%s/results.dat", info.directory().c_str());
+			file = fopen(fname, "a");
+			fprintf(file, "%16.14f %16.14f %16.14f %16.14f %16.14f\n", time, tot_error, num_h, sqrt(params::De*time), max_elem_size/params::ys[1]);
+			fclose(file);
+
+			printf("[%4u] time=%8.6f error=%10.8e iface_err=%10.8e interface=%16.14f expected=%8.6f\n", timestep, time, tot_error, fabs(num_h - sqrt(params::De * time)), num_h, sqrt(params::De * time));
+			printf("interface positions: ");
+			for (unsigned int i = 0; i < geometry->ngeom_data(); i++) printf("%16.14f ", geometry->geom_data_pt(i)->value(0));
+			printf("\n");
+			info.number()++;
+		}
+};
+
+int main(int argc, char **argv) {
+	CommandLineArgs::setup(argc, argv);
+
+	CommandLineArgs::specify_command_line_flag("--k1", &params::solid.k);
+	CommandLineArgs::specify_command_line_flag("--rho1", &params::solid.rho);
+	CommandLineArgs::specify_command_line_flag("--cp1", &params::solid.cp);
+	CommandLineArgs::specify_command_line_flag("--k2", &params::liquid.k);
+	CommandLineArgs::specify_command_line_flag("--rho2", &params::liquid.rho);
+	CommandLineArgs::specify_command_line_flag("--cp2", &params::liquid.cp);
+	CommandLineArgs::specify_command_line_flag("--L", &params::L);
+	CommandLineArgs::specify_command_line_flag("--De", &params::De);
+
+	CommandLineArgs::specify_command_line_flag("--Ts", &params::T_s);
+	CommandLineArgs::specify_command_line_flag("--Tm", &params::T_m);
+	CommandLineArgs::specify_command_line_flag("--Tl", &params::T_l);
+
+	CommandLineArgs::specify_command_line_flag("--nx1", &params::nxs[0]);
+	CommandLineArgs::specify_command_line_flag("--nx2", &params::nxs[1]);
+	CommandLineArgs::specify_command_line_flag("--ny", &params::ny);
+	CommandLineArgs::specify_command_line_flag("--dt", &params::dt);
+	CommandLineArgs::specify_command_line_flag("--tstart", &params::tstart);
+	CommandLineArgs::specify_command_line_flag("--tend", &params::tend);
+	CommandLineArgs::specify_command_line_flag("--write-freq", &params::write_freq);
+	CommandLineArgs::specify_command_line_flag("--dname", &params::dname, "doc");
+
+	CommandLineArgs::parse_and_assign();
+	CommandLineArgs::output();
+
+	params::tsteps = (unsigned int) ((params::tend - params::tstart) / params::dt);
+	printf("tsteps set to %u\n", params::tsteps);
+
+	if (isnan(params::solid.k) || isnan(params::solid.rho) || isnan(params::solid.cp) ||
+		isnan(params::liquid.k) || isnan(params::liquid.rho) || isnan(params::liquid.cp) ||
+		isnan(params::L) || isnan(params::De) || 
+		isnan(params::T_s) || isnan(params::T_m) || isnan(params::T_l)
+	) {
+		
+		printf("Properties not set correctly, exiting...\n\n");
+		exit(1);
+	}
+
+	if (!CommandLineArgs::command_line_flag_has_been_set("--dname")) {
+		char temp[256];
+		sprintf(temp, "RESLT/%dn%u+%u_%dt%.2e", X_ORDER, params::nxs[0], params::nxs[1], T_ORDER, params::dt);
+		params::dname.assign(temp);
+	}
+
+	printf("Saving results to %s\n", params::dname.c_str());
+	if (std::filesystem::exists(params::dname.c_str())) {
+		printf("Directory %s exists\n", params::dname.c_str());
+	} else {
+		std::filesystem::create_directories(params::dname.c_str());
+	}
+
+	char sub_dname[512];
+	sprintf(sub_dname, "%s/steps", params::dname.c_str());
+	if (std::filesystem::exists(sub_dname)) {
+		printf("Directory %s exists\n", sub_dname);
+	} else {
+		std::filesystem::create_directories(sub_dname);
+	}
+
+	DocInfo info;
+	info.set_directory(params::dname.c_str());
+	info.number() = 0;
+
+	char fname[256];
+	sprintf(fname, "%s/results.dat", info.directory().c_str());
+	FILE *file = fopen(fname, "w");
+	fprintf(file, "time error h exact_h area\n");
+	fclose(file);
+
+	printf("Output directory: %s\n", info.directory().c_str());
+
+	params::alpha = (params::liquid.cp * params::liquid.rho) / (params::solid.cp * params::solid.rho);
+	params::beta = params::liquid.k / params::solid.k;
+	params::D = params::beta / params::alpha;
+	params::St = params::L / (params::solid.cp * (params::T_m - params::T_s));
+	params::nx = params::nxs[0] + params::nxs[1];
+	params::xs[1] = sqrt(params::De * params::tstart);
+
+	printf("Problem Def:\n");
+	printf("\tsolid: k=%8.6f rho=%8.6f cp=%8.6f\n", params::solid.k, params::solid.rho, params::solid.cp);
+	printf("\tliquid: k=%8.6f rho=%8.6f cp=%8.6f\n", params::liquid.k, params::liquid.rho, params::liquid.cp);
+	printf("\tL=%8.6f De=%8.6f\n", params::L, params::De);
+	printf("\tTs=%8.6f Tm=%8.6f Tl=%8.6f\n", params::T_s, params::T_m, params::T_l);
+	printf("\talpha=%8.6f beta=%8.6f St=%8.6f\n", params::alpha, params::beta, params::St);
+	printf("\tx0=%8.6f x1=%8.6f x2=%8.6f\n", params::xs[0], params::xs[1], params::xs[2]);
+
+	auto problem = OneDimStefanProblem<QUnsteadyHeatElement<2, X_ORDER>>();
+
+	problem.initialise_dt(params::dt);
+	problem.set_doc_info(info);
+	problem.set_initial_condition();
+
+	int prev_steps = problem.time_stepper_pt()->nprev_values()+1;
+	for (unsigned int t = 0;t < params::tsteps; t++) {
+		problem.unsteady_newton_solve(params::dt);
+
+		if (t % params::write_freq == 0) problem.doc_step(t+prev_steps);
+
+		double h = problem.get_interface();
+		if (h >= params::xs[2] || isnan(h)) {
+			printf("Interface passed out of domain, exiting...\n");
+			break;
+		}
+	}
+
+}
---
+++ one_dim/one_dim/two_phase_free_boundary_mesh.h
@@ -0,0 +1,379 @@
+#ifndef __TWO_PHASE_FREE_BOUNDARY_MESH_H__
+#define __TWO_PHASE_FREE_BOUNDARY_MESH_H__
+
+#include "generic.h"
+#include "unsteady_heat.h"
+
+using namespace oomph;
+
+class FreeBoundaryGeometry : public GeomObject {
+	protected:
+		Vector<Data *> _data_pt;
+		bool destroy_geom_data = false;
+		
+		double _xs[2];
+		double _ys[2];
+
+	public:
+		FreeBoundaryGeometry(
+			double *xs,
+			double *ys,
+			unsigned int nnode,
+			TimeStepper *timestepper
+		) : GeomObject(2, 2, timestepper) {
+			
+			_xs[0] = xs[0];
+			_xs[1] = xs[2];
+			_ys[0] = ys[0];
+			_ys[1] = ys[1];
+
+			_data_pt.resize(nnode);
+
+			for (unsigned int i = 0; i < nnode; i++) {
+				_data_pt[i] = new Data(time_stepper_pt(), 1);
+
+				for (unsigned int t = 0; t < time_stepper_pt()->nprev_values(); t++)
+					_data_pt[i]->set_value(t, 0, xs[1]);
+				
+				_data_pt[i]->pin_all();
+			}
+
+			destroy_geom_data = true;
+		}
+
+		Data *geom_data_pt(const unsigned int &i) {return _data_pt[i];}
+		unsigned int ngeom_data() const {return _data_pt.size();}
+
+		double& x_min() {return _xs[0];}
+		double& x_max() {return _xs[1];}
+		double& y_min() {return _ys[0];}
+		double& y_max() {return _ys[1];}
+
+		double& get_interface(const unsigned int &t, const unsigned int &i) {return *_data_pt[i]->value_pt(t, 0);}
+		double& get_interface(const unsigned int &i) {return get_interface(0, i);}
+
+		void position(const unsigned int &t, const Vector<double> &zeta, Vector<double> &r) const {
+			r[0] = _data_pt[0]->value(0);
+		}
+
+		void position(const Vector<double> &zeta, Vector<double>& r) const {
+			position(0, zeta, r);
+		}
+
+		void pin_free_boundary() {
+			for (unsigned int i = 0; i < ngeom_data(); i++)
+				_data_pt[i]->pin_all();
+		}
+
+		void unpin_free_boundary() {
+			for (unsigned int i = 0; i < ngeom_data(); i++)
+				_data_pt[i]->unpin_all();
+		}
+};
+
+class FreeBoundaryElement : public GeneralisedElement,
+							public FreeBoundaryGeometry {
+	private:
+		unsigned int boundary_index, geometry_index;
+		Vector<unsigned int> node_indices;
+		std::map<unsigned int, unsigned int> node_int_map;
+		std::map<unsigned int, unsigned int> node_ext_map;
+		std::map<Node *, Data *> node_data_map;
+
+	public:
+		FreeBoundaryElement(
+			double *xs,
+			double *ys,
+			unsigned int nnode,
+			TimeStepper *timestepper
+		) : FreeBoundaryGeometry(xs, ys, nnode, timestepper) {
+
+			unpin_free_boundary();
+			destroy_geom_data = true;
+		}
+
+		void add_node(Node *node_pt, unsigned int i) {
+			node_int_map[i] = add_internal_data(geom_data_pt(i));
+			node_ext_map[i] = add_external_data(node_pt);
+			node_data_map[node_pt] = geom_data_pt(i);
+		}
+
+		Data*& get_data_for_node(Node *node_pt) {
+			return node_data_map[node_pt];
+		}
+
+		unsigned int& free_boundary_index() {
+			return boundary_index;
+		}
+
+		void calculate_predicted_values() {
+			for (unsigned int i = 0; i < ngeom_data(); i++) {
+				time_stepper_pt()->calculate_predicted_values(geom_data_pt(i));
+			}
+		}
+	
+	protected:
+		inline void get_residuals(Vector<double> &residuals) {
+			residuals.initialise(0.0);
+			DenseMatrix<double> placeholder(1);
+			fill_in_generic_residual_contribution(residuals, placeholder, false);
+		}
+
+		inline void get_jacobian(Vector<double> &residuals, DenseMatrix<double> &jacobian) {
+			residuals.initialise(0.0);
+			jacobian.initialise(0.0);
+			fill_in_generic_residual_contribution(residuals, jacobian, true);
+		}
+
+		inline void fill_in_generic_residual_contribution(Vector<double> &residuals, DenseMatrix<double> &jacobian, bool compute_jacobian) {
+			if (ndof() == 0) return;
+
+			for (unsigned int i = 0; i < ngeom_data(); i++) {
+				int local_eqn = internal_local_eqn(node_int_map[i], 0);
+				if (local_eqn < 0) continue;
+
+				Node *node_pt = dynamic_cast<Node *>(external_data_pt(node_ext_map[i]));
+				if (node_pt == NULL || !node_pt->is_on_boundary(boundary_index)) continue; 
+
+				residuals[local_eqn] = node_pt->value(0);
+
+				if (compute_jacobian) {
+					int local_unknown = external_local_eqn(node_ext_map[i], 0);
+					if (local_unknown < 0) continue;
+
+					jacobian(local_eqn, local_unknown) = 1.0;
+				}
+			}
+		}
+};
+
+template<class EL>
+class FreeBoundaryFluxElement : public UnsteadyHeatFluxElement<EL> {
+	private:
+		double St;
+		std::map<Node *, int> geom_indices;
+
+	public:
+		FreeBoundaryFluxElement(
+			EL *bulk_elem,
+			unsigned int face_index,
+			double _St
+		) : UnsteadyHeatFluxElement<EL>(bulk_elem, face_index), St(_St) {}
+
+		void add_node_data(Node *node_pt, Data *data_pt) {
+			geom_indices[node_pt] = this->add_external_data(data_pt);
+		}
+	
+	protected:
+		inline void fill_in_contribution_to_residuals(Vector<double> &residuals) {
+			fill_in_generic_residual_contribution_ust_heat_flux(residuals, GeneralisedElement::Dummy_matrix, false);
+		}
+
+		inline void fill_in_contribution_to_jacobian(Vector<double> &residuals, DenseMatrix<double> &jacobian) {
+			fill_in_generic_residual_contribution_ust_heat_flux(residuals, jacobian, true);
+		}
+
+		inline void fill_in_generic_residual_contribution_ust_heat_flux(Vector<double> &residuals, DenseMatrix<double> &jacobian, bool compute_jacobian) {
+			if (this->ndof() == 0) return;
+
+			Vector<double> s(1), n(2);
+			Shape phi(this->nnode()), psi(this->nnode());
+			
+			for (unsigned int ipt = 0; ipt < this->integral_pt()->nweight(); ipt++) {
+				for (unsigned int i = 0; i < s.size(); i++) s[i] = this->integral_pt()->knot(ipt, i);
+
+				double J = this->shape_and_test(s, phi, psi);
+				double W = J * this->integral_pt()->weight(ipt);
+				this->outer_unit_normal(s, n);
+				double n_mag = sqrt(n[0]*n[0] + n[1]*n[1]);
+				printf("normal: n0=%16.14f n1=%16.14f mag=%16.14f\n", n[0], n[1], n_mag);
+				if (n_mag - 1.0 > 1e-6) {
+					printf("WARNING: normal not quite a unit\n\n\n");
+				}
+
+
+				for (unsigned int n = 0; n < this->nnode(); n++) {
+					int local_eqn = this->nodal_local_eqn(n, 0);
+					if (local_eqn < 0) continue;
+
+					Node *node = this->node_pt(n);
+					Data *geom = this->external_data_pt(geom_indices[node]);
+
+					double dhdt = geom->time_stepper_pt()->time_derivative(1, geom, 0);
+					residuals[local_eqn] -= phi(n) * St * dhdt * W;
+
+					if (compute_jacobian) {
+						int h_eqn = this->external_local_eqn(geom_indices[node], 0);
+						if (h_eqn < 0) continue;
+
+						jacobian(local_eqn, h_eqn) -= phi(n) * St * geom->time_stepper_pt()->weight(1, 0) * W;
+					}
+				}
+			}
+		}
+};
+
+template<class EL>
+class TwoPhaseFreeBoundarySpineMesh : public RectangularQuadMesh<EL>,
+									  public SpineMesh {
+	private:
+		const unsigned int _free_boundary_index = 4;
+		const unsigned int nx1, nx2, nx, ny;
+
+		FreeBoundaryElement *_geometry;
+	public:
+		TwoPhaseFreeBoundarySpineMesh(
+			unsigned int *nxs,
+			unsigned int ny,
+			double *xs,
+			double *ys,
+			TimeStepper *timestepper
+		) : RectangularQuadMesh<EL>(nxs[0]+nxs[1], ny, xs[0], xs[2], ys[0], ys[1], timestepper),
+			nx1(nxs[0]), nx2(nxs[1]), nx(nxs[0]+nxs[1]), ny(ny) {
+
+			this->set_nboundary(5);
+			
+			for (unsigned int e = 0; e < ny; e++) {
+				EL *elem = dynamic_cast<EL *>(this->element_pt((nxs[0]+nxs[1])*e + nxs[0]));
+				unsigned int nnode_1d = elem->nnode_1d();
+				for (unsigned int n = 0; n < nnode_1d; n++) {
+					Node *node = elem->node_pt(n*nnode_1d);
+					
+					this->convert_to_boundary_node(node);
+					this->add_boundary_node(_free_boundary_index, node);	
+				}
+			}
+			
+			unsigned int nnode_freeboundary = nboundary_node(_free_boundary_index);
+			_geometry = new FreeBoundaryElement(xs, ys, nnode_freeboundary, timestepper);
+			_geometry->free_boundary_index() = _free_boundary_index;
+			for (unsigned int n = 0; n < nnode_freeboundary; n++) {
+				Node *node = boundary_node_pt(_free_boundary_index, n);
+				_geometry->add_node(node, n);
+			}
+
+			construct_spines(xs[1]);
+
+			for (unsigned int n = 0; n < nnode(); n++) {
+				spine_node_update(node_pt(n));
+			}
+		}
+
+		FreeBoundaryElement* geometry() const {
+			return _geometry;
+		}
+
+		Spine *_create_new_spine(unsigned int s, const double h) {
+			Spine *spine = new Spine(h);
+			// spine->spine_height_pt()->pin(0);
+			spine->spine_height_pt()->pin_all();
+			Spine_pt.push_back(spine);
+			spine->add_geom_data_pt(_geometry->geom_data_pt(s));
+
+			return spine;
+		}
+
+		void _add_spine_to_node(
+			const unsigned int e, 
+			const unsigned int n, 
+			const double fraction,
+			const unsigned int fct_id,
+			Spine *spine
+		) {
+			SpineNode *node_pt = element_node_pt(e, n);
+			node_pt->spine_pt() = spine;
+			node_pt->fraction() = fraction;
+			node_pt->spine_mesh_pt() = this;
+			node_pt->node_update_fct_id() = fct_id;
+		}
+
+		void construct_spines(double h) {
+			unsigned int nnode_1d = finite_element_pt(0)->nnode_1d();
+			unsigned int nspine = nboundary_node(_free_boundary_index);
+			Spine_pt.reserve(nspine);
+
+			unsigned int yi;
+			for (yi = 0; yi < ny-1; yi++) {
+				for (unsigned int s = 0; s < nnode_1d-1; s++) {
+					Spine *spine = _create_new_spine(s, h);
+
+					unsigned int xi;
+					for (xi = 0; xi < nx1-1; xi++) {
+						for (unsigned int n = 0; n < nnode_1d-1; n++) {
+							_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)xi + (double)n/(double)(nnode_1d-1))/(double)nx1, 0, spine);
+						}
+					}
+
+					xi = nx1-1;
+					for (unsigned int n = 0; n < nnode_1d; n++) {
+						_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)xi + (double)n/(double)(nnode_1d-1))/(double)nx1, 0, spine);
+					}
+
+					for (xi = nx1; xi < nx-1; xi++) {
+						for (unsigned int n = 0; n < nnode_1d-1; n++) {
+							_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)(xi-nx1) + (double)n/(double)(nnode_1d-1))/(double)nx2, 1, spine);
+						}
+					}
+
+					xi = nx-1;
+					for (unsigned int n = 0; n < nnode_1d; n++) {
+						_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)(xi-nx1) + (double)n/(double)(nnode_1d-1))/(double)nx2, 1, spine);
+					}
+				}
+			}
+
+			yi = ny-1;
+			for (unsigned int s = 0; s < nnode_1d; s++) {
+				Spine *spine = _create_new_spine(s, h);
+
+				unsigned int xi;
+				for (xi = 0; xi < nx1-1; xi++) {
+					for (unsigned int n = 0; n < nnode_1d-1; n++) {
+						_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)xi + (double)n/(double)(nnode_1d-1))/(double)nx1, 0, spine);
+					}
+				}
+
+				xi = nx1-1;
+				for (unsigned int n = 0; n < nnode_1d; n++) {
+					_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)xi + (double)n/(double)(nnode_1d-1))/(double)nx1, 0, spine);
+				}
+
+				for (xi = nx1; xi < nx-1; xi++) {
+					for (unsigned int n = 0; n < nnode_1d-1; n++) {
+						_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)(xi-nx1) + (double)n/(double)(nnode_1d-1))/(double)nx2, 1, spine);
+					}
+				}
+
+				xi = nx-1;
+				for (unsigned int n = 0; n < nnode_1d; n++) {
+					_add_spine_to_node(yi*nx + xi, s*nnode_1d+n, ((double)(xi-nx1) + (double)n/(double)(nnode_1d-1))/(double)nx2, 1, spine);
+				}
+			}
+
+			printf("Created %lu/%u spines\n\n", Spine_pt.size(), nspine);
+		}
+
+		const unsigned int& free_boundary_index() const {
+			return _free_boundary_index;
+		}
+
+		void spine_node_update(SpineNode * node_pt) {
+			Data *geom_data = node_pt->spine_pt()->geom_data_pt(1);
+
+			double h = geom_data->value(0);
+			double frac = node_pt->fraction();
+
+			double x0 = _geometry->x_min();
+			double x1 = _geometry->x_max();
+
+			switch (node_pt->node_update_fct_id()) {
+				case 0: node_pt->x(0) = x0 + frac * (h - x0); break;
+				case 1: node_pt->x(0) = h  + frac * (x1 - h); break;
+				default:
+					printf("Invalid node updating function\n\n");
+					break;
+			}
+		}
+};
+
+#endif
