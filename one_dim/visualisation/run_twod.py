83b087a7d145deea850c8abdb73b2b7fcfe4645d
Rumesh Sudhaharan <rumesh986@gmail.com>
2026-03-04 10:23:58 +0000

    add more plotting functions for 2D

--- one_dim/visualisation/run_twod.py
+++ one_dim/visualisation/run_twod.py
@@ -1,7 +1,9 @@
 from typing import override
+from warnings import warn
 
 import numpy as np
 import polars as pl
+from scipy.optimize import curve_fit
 from matplotlib.pyplot import Axes, Figure
 from matplotlib.animation import Animation, FuncAnimation
 
@@ -25,10 +27,18 @@ class Run_TwoD(RunBase):
 	def interface_error_norm(self) -> float:
 		pass
 
-	@property
 	@override
-	def fitted_De(self) -> float | None:
-		pass
+	def fitted_De(self, yi: str) -> float | None:
+		def f(x, a, b):
+			return np.sqrt(x * a) + b
+
+		try:
+			optimal, _ = curve_fit(f, self.interface[self.COLS.time], self.interface[yi], p0=[1.0, 0.0])
+			de = optimal[0]
+			return de
+		except Exception as e:
+			print(f"Failed to fit De for {self.params.title}, error: {e}")
+		
 
 	@override
 	def plot_error_norm(self, ax: Axes, *args, **kwargs) -> None:
@@ -40,7 +50,13 @@ class Run_TwoD(RunBase):
 
 	@override
 	def plot_interface(self, ax: Axes, de: bool = True, label: str = 'Interface location', *args, **kwargs) -> None:
-		pass
+		for yi in self.interface.columns[2:]:
+			de = self.fitted_De(yi)
+			ax.plot(self.interface[self.COLS.time], self.interface[yi], label=f'y={float(yi):.3f} de={de:e}')
+
+			print(f'y={yi} de={de}')
+		
+		ax.legend()
 
 	@override
 	def plot_interface_error(self, abs_ax: Axes, *args, **kwargs) -> None:
@@ -52,12 +68,20 @@ class Run_TwoD(RunBase):
 
 	@override
 	def plot_mesh(self, ax: Axes, step: int, cmap: str = 'jet', *args, **kwargs) -> None:
-		pass
+		frame = self.step(step)
+		iface = self.interface.filter(step=step)
+
+		plot = ax.scatter(frame[self.COLS.x], frame[self.COLS.y], c=frame[self.COLS.temp], cmap=cmap)
+		ax.plot(iface.row(0)[2:], [float(i) for i in iface.columns[2:]], label='Interface', c='black', ls='--')
+
+		ax.set_title(f't={self.results[step, "time"]:.4f}')
+
+		return plot
 
 	@override
 	def plot_profile(self, ax: Axes, step: int, y_idx: inst = 0, markerstep: int = 10, *args, **kwargs) -> None:
-		frame = self.steps.filter(pl.col(self.COLS.step).eq(step))
-		iface = self.interface.filter(pl.col(self.COLS.step).eq(step))
+		frame = self.step(step)
+		iface = self.interface.filter(step=step)
 
 		xs = iface.row(0)[2:]
 		ys = tuple([float(i) for i in iface.columns[2:]])
@@ -65,8 +89,49 @@ class Run_TwoD(RunBase):
 		ax.tricontourf(frame[self.COLS.x], frame[self.COLS.y], frame[self.COLS.temp])
 		ax.plot(xs, ys, label='Interface', c='black')
 
+		ax.set_title(f't={self.results[step, self.COLS.time]:.4f}')
 
 
 	@override
 	def anim_profile(self, fig: Figure, prof: Axes | None = None, error: Axes | None = None, nodes: int | None = 15, zoom: bool = False, *args, **kwargs) -> Animation:
-		pass
+		def _update(step):
+			for time in times:
+				time.set_text(f't={self.results[step, self.COLS.time]:.4f}')
+
+			if prof is not None:
+				frame = self.step(step)
+				iface = self.interface.filter(step=step)
+
+				prof_lines['contour'].remove()
+
+				prof_lines['contour'] = prof.tricontourf(frame[self.COLS.x], frame[self.COLS.y], frame[self.COLS.temp], cmap='jet', levels=40)
+				prof_lines['iface'].set_data(iface.row(0)[2:], [float(i) for i in iface.columns[2:]])
+
+		times = []
+
+		if prof is not None:
+			prof_lines = {}
+			times.append(prof.annotate(
+				f't={self.results[0, self.COLS.time]:.4f}',
+				xy=(0.6, 0.9),
+				xycoords='axes fraction'
+			))
+
+			frame = self.step(0)
+			iface = self.interface.filter(step=0)
+			prof_lines['contour'] = prof.tricontourf(frame[self.COLS.x], frame[self.COLS.y], frame[self.COLS.temp], cmap='jet', levels=40)
+			prof_lines['iface'], = prof.plot(iface.row(0)[2:], [float(i) for i in iface.columns[2:]], ls='--', c='black')
+
+			prof.set_ylabel('y')
+			prof.set_xlabel('x')
+		
+		fig.colorbar(prof_lines['contour'])
+
+		if 'anim_length' in kwargs:
+			warn("Animation length control has not been implemented yet")
+
+		return FuncAnimation(
+			fig, 
+			_update,
+			self.results.shape[0]
+		)
